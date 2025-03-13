ecc74ae1d931c9ffd20a1d8543598ac89ea399c5
Manuela Estrada <manuesmanager@gmail.com>
2025-03-12 23:26:41 -0500

    Controlador computadores

---
+++ Computadores/Computadores/Controllers/ComputadoresController.cs
@@ -0,0 +1,109 @@
+﻿using Computadores.Clases;
+using Computadores.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Computadores.Controllers
+{
+    [RoutePrefix("api/Computadores")]
+    public class ComputadoresController : ApiController
+    {
+        [HttpGet]
+        [Route("ConsultarTodos")]
+
+        public List<Computador> ConsultarTodos()
+        {
+            try
+            {
+                clsComputador clsComputador = new clsComputador();
+                return clsComputador.ConsultarTodos();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultarXCodigo")]
+        public Computador ConsultarXCodigo(int codigo)
+        {
+            try
+            {
+                clsComputador clsComputador = new clsComputador();
+                return clsComputador.Consultar(codigo);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultarXTipo")]
+        public List<Computador> ConsultarXTipo(int codigoTipo)
+        {
+            try
+            {
+                clsComputador clsComputador = new clsComputador();
+                return clsComputador.ConsultarPorTipo(codigoTipo);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Computador computador)
+        {
+            try
+            {
+                clsComputador clsComputador = new clsComputador();
+                clsComputador.computador = computador;
+                return clsComputador.Insertar();
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar computador: " + ex.Message;
+            }
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Computador computador)
+        {
+            try
+            {
+                clsComputador clsComputador = new clsComputador();
+                clsComputador.computador = computador;
+                return clsComputador.Actualizar();
+            }
+            catch (Exception ex)
+            {
+                return "Error al actualizar computador: " + ex.Message;
+            }
+        }
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar([FromBody] Computador computador)
+        {
+            try
+            {
+                clsComputador clsComputador = new clsComputador();
+                clsComputador.computador = computador;
+                return clsComputador.Eliminar();
+            }
+            catch (Exception ex)
+            {
+                return "Error al eliminar computador: " + ex.Message;
+            }
+        }
+    }
+}
